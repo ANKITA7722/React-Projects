dc1cd24c434b4a52a88b0d1aef8c4b96dc31a3e0
ANKITA7722 <ANKITA7722>
2024-09-17 11:47:51 -0230

    Start react js Practicing

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Cybrome from "./Cybrome";
+
+ const App=()=>{
+    return(
+        <p>
+        <h1> Hii i am Ankita from bhopal</h1>
+        <Cybrome/>
+        </p>
+    )
+}
+export default App;
---
+++ src/Cybrome.jsx
@@ -0,0 +1,9 @@
+ const Cybrome=()=>{
+    return(
+        <p>
+        <h1>ankita</h1>
+        </p>
+
+    )
+ }
+ export default Cybrome;
