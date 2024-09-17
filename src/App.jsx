c6fa9dc3dd43a71a5dc79691ebcd05f70333236b
ANKITA7722 <ANKITA7722>
2024-09-17 12:22:29 -0230

    add new para and change address

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Cybrome from "./Cybrome";
  const App=()=>{
     return(
         <p>
-        <h1> Hii i am Ankita from bhopal</h1>
+        <h1> Hii i am Ankita from waghoda</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia enim, ipsum voluptatum ipsam aperiam consequatur vel sint dignissimos.
+            g Natus praesentium id beatae enim nam, magni doloribus fuga voluptas incidunt itaque.</p>
         <Cybrome/>
         </p>
     )
