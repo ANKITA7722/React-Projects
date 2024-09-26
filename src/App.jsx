f1be30bff98dbe089041c27d24e54fb87fcee241
ANKITA7722 <bobdeankita24@gmail.com>
2024-09-26 06:37:24 -0230

    row collum

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,46 @@
-import Carousel from 'react-bootstrap/Carousel';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
- import img1 from "./images/img1.jpg";
- import img2 from "./images/img2.jpg";
- import img3 from "./images/img3.jpg";
+import Carousel from 'react-bootstrap/Carousel';
+ import img1 from "./images/op1.jpg";
+ import img2 from "./images/op2.jpg";
+ import img3 from "./images/op1.jpg";
+
+//  import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 
 const App=()=>{
     return(
     <>
-   
-   <Carousel>
-      <Carousel.Item>
-        {/* image1 */}
-        <img src={img1} width={"100%"}/>
 
-
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        {/* img2 */}
-      <img src={img2 } width={"100%"}/>
-
-
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
+<Container>
+      <Row>
+        <Col md={2} className='bg-primary'>ankita</Col>
+        <Col md={2} className='bg-success'>gauri</Col>
+        <Col md={3} className='bg-warning'>atmik</Col>
+        <Col md={5} className='bg-danger'>1 of 1</Col>
+      </Row>
+        <br/>
+      <Row>
+      <Col md={2} className='bg-primary'>ankita</Col>
+        <Col md={2} className='bg-success'>gauri</Col>
+        <Col md={4} className='bg-warning'>1 of 1</Col>
+        <Col md={2} className='bg-danger'>1 of 1</Col>
+        <Col md={2} className='bg-warning'>1 of 1</Col>
         
-      <img src={img3 } width={"100%"} />
+      </Row>
+    </Container>
 
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-
+   
+   
 
     </>
     )
 }
 export default App;
+
